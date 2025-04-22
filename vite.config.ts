8260977b20642d5954fcc8e62607b96ae064fa14
Jack-The-Ripper-1820 <mayurjreddy@gmail.com>
2025-04-22 09:12:27 -0500

    vite config include base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
+  // Base public path when served from GitHub Pages under /Arcade-Shooter-ThreeJS/
+  base: '/Arcade-Shooter-ThreeJS/',
   plugins: [wasm()],
   build: {
     target: 'esnext',
